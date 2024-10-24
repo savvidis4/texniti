2f1c60e59a292c46bd6cf40e76b253a8d94995cf
savvidis4 <43894385+savvidis4@users.noreply.github.com>
2024-10-24 14:15:52 +0000

    sxolia

--- search_algorithm_front_queue_with_left_operator pacman.py
+++ search_algorithm_front_queue_with_left_operator pacman.py
@@ -16,102 +16,189 @@ import sys
 
 """ Helper functions for checking operator's conditions """
 
+# Έλεγχος για το αν μπορεί το Pacman να φάει στο κελί που βρίσκεται.
 def can_eat(state):
+
+    # Σειριακή αναζήτηση σε όλη την υπάρχουσα κατάσταση.
     for i in range(len(state)):
+
+        # Άμα στο κελί βρίσκεται το Pacman,
         if state[i][0] == 'p':
+
+            # Αν υπάρχει είτε καλό είτε κακό φρούτο,
             if state[i][1] == 'f' or state[i][1] == 'd':
+
+                # Μπορεί να φάει το εκάστοτε φρούτο.
                 return True
+    
+    # Δεν υπάρχει φρούτο στο κελί που βρίσκεται το Pacman οπότε δεν μπορείς να φάει.
     return False
       
-
+# Έλεγχος για το αν μπορεί το Pacman να κουνηθεί ένα βήμα δεξιά.
 def can_move_right(state):
+
+    # Άμα δεν βρίσκεται το Pacman στο τελευταίο κελί της υπάρχουσας κατάστασης τότε μπορεί.
     return not state[5][0]=='p'
 
+# Έλεγχος για το αν μπορεί το Pacman να κουνηθεί ένα βήμα αριστερά.
 def can_move_left(state):
+
+    # Άμα δεν βρίσκεται το Pacman στο πρώτο κελί της υπάρχουσας κατάστασης τότε μπορεί.
     return not state[0][0]=='p'
 
 
 """ Operator function: eat, move right, move left """
 
+# Τελεστής eat.
 def eat(state):
+
+    # Αν το Pacman μπορεί να φάει,
     if can_eat(state):
 
+        # Αρχικοποίηση κενής λίστας θέσεως των φρούτων και ενός μετρητή.
         fruits_list = []
         k = 0
         random_index = 0
 
+        # Σειριακή αναζήτηση του κελιού που βρίσκεται το Pacman στην τρέχουσα κατάσταση.
         for i in range(len(state)):
 
+            # Άμα το Pacman βρεθεί,
             if state[i][0] == 'p':
-                #prwta pame sto keli ta emfanizoume kai ta duo mazi (p,f) kai se epomeno step trwme? 
-                #h me to pou paei sto keli automata to fruit exafanizete?
+
+                # Άμα το φρούτο είναι καλό φρούτο,
                 if state[i][1] == 'f':
+
+                    # Φάε το φρούτο (εξαφάνισέ το).
                     state[i][1] = ''
+                
+                # Άμα το φρούτο είναι κακό φρούτο,
                 elif state[i][1] == 'd':
+
+                    # Κατάστρεψε το φρούτο (εξαφάνισέ το),
                     state[i][1] = ''
 
+                    # Σειριακή αναζήτηση οντοτήτων στην τρέχουσα κατάσταση.
                     while (k < 6):
-                        #Mporei na emfanistei frouto mazi h dipla sto pacman
+
+                        # Άμα το κελί περιέχει φρούτο (f) ή το Pacman,
                         if (state[k][1] == 'f' or state[k][1] == 'd') or state[k][0] == 'p':
+
+                            # Αποθήκευσε την θέση του κελιού στην λίστα θέσεων οντοτήτων.
                             fruits_list.append(k)
                         k += 1
 
+                    # Έλεγχος θέσεως τυχαίου φρούτου για να μην τοποθετηθεί σε κελί με κάποια υπάρχουσα οντότητα.
                     while (random_index in fruits_list):
+
+                        # Αναπαραγωγή τυχαίου αριθμού από το 0 έως το 5 που θα συμβολίζει την θέση ενός κελιού της κατάστασης.
                         random_index = random.randint(0,5)
 
+                    # Αφού ο τυχαίος "δείκτης" δεν αντιστοιχεί σε κελί με οντότητα, βάλε στο κελί που δείχνει ένα νέο φρούτο.
                     state[random_index][1] = 'f'
         
+        # Επιστροφή κατάστασης μετά το φάγωμα ενός φρούτου.
         return state
     
+    # Άμα δεν υπάρχει φρούτο και το Pacman δεν μπορεί να φάει επιστρέφουμε Κενό (None).
+    return None
+    
+# Τελεστής move_right. 
+def move_right(state):
+
+    # Άμα το Pacman μπορεί να κουνηθεί μία θέση δεξιά,
+    if can_move_right(state):
+
+        # Σειριακή αναζήτηση του Pacman στην τρέχουσα κατάσταση.
+        for i in range(len(state)):
+
+            # Άμα βρήκαμε το Pacman
+            if state[i][0]=='p':
+
+                # Βγάλτο από το κελί του,
+                state[i][0]=''
+
+                # και μετέφερε το στο επόμενο στα δεξιά.
+                state[i+1][0]='p'
+
+                # Επέστρεψε την τρέχουσα κατάσταση μετά την μετακίνηση του Pacman.
+                return state
+
+    # Άμα δεν μπορεί να κουνηθεί δεξιά (π.χ. διότι έχει πετύχει "τοίχο") επιστρέφουμε Κενό (None).
     return None
     
 
-def move_right(state):
-    if can_move_right(state):
-        for i in range(len(state)):
-            if state[i][0]=='p':
-                state[i][0]=''
-                state[i+1][0]='p'
-                return state
-    else:
-        return None
-    
-
+# Τελεστής move_left.
 def move_left(state):
+
+    # Άμα το Pacman μπορεί να κουνηθεί μία θέση αριστερά,
     if can_move_left(state):
+
+        # Σειριακή αναζήτηση του Pacman στην τρέχουσα κατάσταση.
         for i in range(len(state)):
+
+            # Άμα βρήκαμε το Pacman
             if state[i][0]=='p':
+
+                # Βγάλτο από το κελί του,
                 state[i][0]=''
+
+                # και μετέφερε το στο επόμενο στα αριστερά.
                 state[i-1][0]='p'
+
+                # Επέστρεψε την τρέχουσα κατάσταση μετά την μετακίνηση του Pacman.
                 return state
-    else:
-        return None
+
+    # Άμα δεν μπορεί να κουνηθεί δεξιά (π.χ. διότι έχει πετύχει "τοίχο") επιστρέφουμε Κενό (None). 
+    return None
+
 
 """ Function that checks if current state is a goal state """
 
+# Έλεγχος άμα η τρέχουσα κατάσταση είναι τελική.
 def is_goal_state(state):
 
+    # Για να είναι μία κατάσταση τελική θα πρέπει να μήν υπάρχει ΚΑΜΙΑ άλλη οντότητα εκτός του Pacman.
     for cell in state:
+
+        # Κοινώς ελέγχουμε όλο τον κόσμο του προβλήματος για το αν υπάρχουν φρούτα (δηλητηριώδη και μη).
         if cell[1] != '':
+
+            # Άμα βρεθεί φρούτο, επιστρέφουμε πως η κατάσταση ΔΕΝ είναι τελική.
             return False
+    
+    # Άμα δεν βρεθεί φρούτο επιστρέφουμε το Ο.Κ.
     return True
 
     
 
 """ Function that finds the children of current state """
 
+# Συνάρτηση εύρεσης απογόνων.
 def find_children(state, method):
+
+    # Αρικοποίηση λίστας απογόνων
     children=[]
     
+    # Δημιουργία αντιγράφων της τρέχουσας κατάστασης. 
     eat_state=copy.deepcopy(state)
     left_state=copy.deepcopy(state)
     right_state=copy.deepcopy(state)
 
+    # Δημιουργία απογόνων με βάση τους τελεστές κατάστασης.
     child_eat=eat(eat_state)
     child_left=move_left(left_state)
     child_right=move_right(right_state)
 
+    # Καθώς ο κόσμος του προβλήματος είναι ο συγκεκριμένος έχουμε κάποιους περιορισμούς για την σειρά προτεραιότητας των απογόνων μέσα στην λίστα.
+    # Δηλαδή άμα το Pacman βρεθεί σε ίδιο κελί με φρούτο, πρέπει ΠΡΩΤΑ να το φάει κι ύστερα να κουνιθεί.
+    # Δεν επιτρέπεται να το προσπεράσει και να επιστρέψει να το φάει αργότερα.
+
+    # Για αυτόν τον λόγο η σειρά με την οποία θα βάλουμε τους απογόνους στην λίστα απογόνων διαφέρει από μέθοδο σε μέθοδο.
+
     if method == 'DFS':
+
+        # Άμα η 
 
         if not child_left==None:
             children.append(child_left) 
